ad91619e11bdc3e6c02eabbe3f08b0368eb1712a
AditiKadam-09 <aditikadam2005@gmail.com>
2024-12-06 16:53:22 +0530

    output

--- sumNum.java
+++ sumNum.java
@@ -1,8 +1,9 @@
-public class sumNum{
-  public static void main(String args[]){
+public class sumNum {
+  public static void main(String args[]) {
     System.out.println("Addition Program");
-    int a=10,b=20;
-    int c=a+b;
-    System.out.println(a+"+"+b+"="+c);
+    int a = 10, b = 20;
+    int c = a + b;
+    System.out.println(" o utp u t   :");
+    System.out.println(a + "+" + b + "=" + c);
   }
 }
